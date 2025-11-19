9cfcea972d54522106219482d53bed6b72823495
UMA SRINU <umasrinupampana125890@gmail.com>
2025-11-19 20:21:07 +0530

    Add files via upload

---
+++ subtraction of two number.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+ {
+    int a,b,subtraction;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+  subtraction = a - b;
+    printf("Subtraction = %d\n",subtraction);
+    return 0;
+}

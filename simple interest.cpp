67e8541dd5c0c7dd51a1f9b666b3f9069f9c9665
UMA SRINU <umasrinupampana125890@gmail.com>
2025-11-19 20:21:49 +0530

    Add files via upload

---
+++ simple interest.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+ {
+    float principal, rate, time, simpleInterest;
+    printf("Enter principal amount: ");
+    scanf("%f", &principal);
+    printf("Enter rate of interest: ");
+    scanf("%f", &rate);
+    printf("Enter time period (in years): ");
+    scanf("%f", &time);
+    simpleInterest = (principal * rate * time) / 100;
+    printf("Simple Interest = %.2f\n", simpleInterest);
+    return 0;
+}
+

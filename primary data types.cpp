67ce3288e07a42432df8511227c7f3c94f641f4f
UMA SRINU <umasrinupampana125890@gmail.com>
2025-11-23 13:56:20 +0530

    Add files via upload

---
+++ primary data types.cpp
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main()
+{
+	int x=10;
+	printf("x=%d",x);
+	char ch='a';
+	printf("\n ch=%c",ch);
+	float pi=3.14;
+	printf("\n pi=%f",pi);
+	double p=12345.0679;
+	printf("\n p=%lf",p);
+	long int q=6563523;
+	printf("\n q=%d",q);
+	return 0;
+}

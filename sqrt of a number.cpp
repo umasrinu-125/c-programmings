4b03d2daeb7637be26dbd32b41d00c68ef7c994a
UMA SRINU <umasrinupampana125890@gmail.com>
2025-11-23 13:54:40 +0530

    Add files via upload

---
+++ sqrt of a number.cpp
@@ -0,0 +1,11 @@
+# include <stdio.h>
+#include<math.h>
+int main()
+{
+	float x,y;
+	printf("enter x value");
+	scanf("%f",&x);
+	y=(sqrt(x));
+	printf("square root of a given number=%f",y);
+	return 0;
+}
